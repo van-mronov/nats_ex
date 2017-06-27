ea584a8de987426f14488b095e9fc42a37e44bab
Ivan Mironov <ivan-mironov@binomo.com>
2017-06-27 13:04:21 +0300

    Update gproc dep to 0.6.1

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule NatsEx.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:poison, "~> 2.2"},
-     {:gproc, "~> 0.5.0"},
+     {:gproc, "~> 0.6.1"},
      {:ex_doc, "~> 0.12", only: :dev},
      {:credo, "~> 0.4", only: [:dev, :test]}]
   end
