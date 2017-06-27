c5164b7023d626de07f12d341bde784ca6daf9ca
Ivan Mironov <ivan-mironov@binomo.com>
2017-06-27 14:39:38 +0300

    Update to poison 3.1

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule NatsEx.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, "~> 2.2"},
+    [{:poison, "~> 3.1"},
      {:gproc, "~> 0.6.1"},
      {:ex_doc, "~> 0.12", only: :dev},
      {:credo, "~> 0.4", only: [:dev, :test]}]
